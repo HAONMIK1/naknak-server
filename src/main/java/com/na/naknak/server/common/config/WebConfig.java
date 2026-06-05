2a65d0cfd023b97a7c40a1d5c7d11454da432efa
김노아 <noa2006@naver.com>
2026-06-06 02:23:05 +0900

    feat: @LoginUser 어노테이션 구현

--- src/main/java/com/na/naknak/server/common/config/WebConfig.java
+++ src/main/java/com/na/naknak/server/common/config/WebConfig.java
@@ -1,19 +1,29 @@
 package com.na.naknak.server.common.config;
 
+import com.na.naknak.server.common.auth.LoginUserArgumentResolver;
 import com.na.naknak.server.common.logging.TraceIdInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final TraceIdInterceptor traceIdInterceptor;
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(traceIdInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
     }
 }
