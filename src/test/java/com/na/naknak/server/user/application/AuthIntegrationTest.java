51bee3ae57863705f385564366778637a4a09f0b
김노아 <noa2006@naver.com>
2026-06-06 02:27:43 +0900

    test: 인증 통합 테스트 시나리오 정의

---
+++ src/test/java/com/na/naknak/server/user/application/AuthIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.na.naknak.server.user.application;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthIntegrationTest {
+    @Test
+    void 신규_유저_로그인_NEED_SIGNUP_반환() {}
+
+    @Test
+    void 회원가입_성공_토큰_반환() {}
+
+    @Test
+    void 회원가입_후_로그인_AUTHENTICATED_반환() {}
+
+    @Test
+    void 토큰_재발급_성공() {}
+
+    @Test
+    void 로그아웃_후_블랙리스트_등록() {}
+}
