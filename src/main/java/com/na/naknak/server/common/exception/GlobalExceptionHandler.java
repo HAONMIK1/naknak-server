9a26cc144470e1750e30b50886b6f63eae111187
김노아 <noa2006@naver.com>
2026-06-07 07:49:20 +0900

    fix: GlobalExceptionHandler에 400/405 예외 처리 추가

--- src/main/java/com/na/naknak/server/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/na/naknak/server/common/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import com.na.naknak.server.common.ApiResponse;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,16 +29,30 @@ public class GlobalExceptionHandler {
                 .body(ApiResponse.fail(message));
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiResponse<Void>> handleHttpMessageNotReadable(HttpMessageNotReadableException e) {
         return ResponseEntity
-                .internalServerError()
-                .body(ApiResponse.fail("서버 오류가 발생했습니다."));
+                .badRequest()
+                .body(ApiResponse.fail("잘못된 요청 형식입니다."));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ApiResponse<Void>> handleMethodNotSupported(HttpRequestMethodNotSupportedException e) {
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(ApiResponse.fail("지원하지 않는 HTTP 메서드입니다."));
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ApiResponse<Void>> handleDataIntegrityViolation(DataIntegrityViolationException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(ApiResponse.fail("이미 사용 중인 값입니다."));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+        return ResponseEntity
+                .internalServerError()
+                .body(ApiResponse.fail("서버 오류가 발생했습니다."));
     }
 }
