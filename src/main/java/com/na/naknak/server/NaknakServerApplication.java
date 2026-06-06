05a1028d55f4635fe03fd42382101eb460f7e603
김노아 <noa2006@naver.com>
2026-06-07 08:15:28 +0900

    refactor: @EnableJpaAuditing를 JpaConfig로 분리하여 WebMvcTest 호환성 확보

--- src/main/java/com/na/naknak/server/NaknakServerApplication.java
+++ src/main/java/com/na/naknak/server/NaknakServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@EnableJpaAuditing
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class NaknakServerApplication {
 	public static void main(String[] args) {
